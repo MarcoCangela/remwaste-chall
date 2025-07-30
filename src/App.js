b343fa612eeb8c5d71191e17fc8e2b39e751cbe7
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2025-07-30 23:31:38 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AuthProvider } from './context/AuthContext';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+  );
+}
